2e8beed49bb2a04668ddb8a51ae6f6ac96500505
santanka <mkrock1062@gmail.com>
2024-06-30 20:02:13 +0900

    psi_theta_trajectory.pyを改造

--- single_test_particle/keisan/psi_theta_trajectory.py
+++ single_test_particle/keisan/psi_theta_trajectory.py
@@ -8,11 +8,10 @@ mpl.rcParams['text.usetex'] = True
 mpl.rcParams['font.family'] = 'serif'
 mpl.rcParams['font.serif'] = ['Computer Modern Roman']
 mpl.rcParams['mathtext.fontset'] = 'cm'
-plt.rcParams["font.size"] = 40
 
 #定数
-#S_number_array = [0E0, 2.5E-1, 5E-1, 7.5E-1, 1E0, 1.25E0]
-S_number_array = [0E0, 2.5E-1, 5E-1, 1E0]
+S_number_array = [0E0, 2.5E-1, 5E-1, 7.5E-1, 1E0, 1.25E0]
+#S_number_array = [0E0, 2.5E-1, 5E-1, 1E0]
 
 # wave_phaseの範囲
 wave_phase_min = -np.pi
@@ -32,9 +31,14 @@ psi_pi_theta_num = 16
 
 
 # plot
-square_num = np.ceil(np.sqrt(len(S_number_array)))
-square_num = int(square_num)
-fig = plt.figure(figsize=(square_num*5, square_num*5), dpi=100)
+#len(S_number_array)を因数分解して、中央の値に近い長方形または正方形になるように調整
+factor = np.array([i for i in range(1, len(S_number_array) + 1) if len(S_number_array) % i == 0])
+if len(factor) % 2 == 0:
+    vertical_number, horizontal_number = factor[len(factor)//2 - 1], factor[len(factor)//2]
+else:
+    vertical_number, horizontal_number = factor[len(factor)//2], factor[len(factor)//2]
+square_num = np.sqrt(len(S_number_array))
+fig = plt.figure(figsize=(horizontal_number*5, vertical_number*5), dpi=100)
 
 plt.rcParams["font.size"] = square_num * 10
 #ax_1 = fig.add_subplot(231, xlabel=r'$\psi$ [$\pi$ rad]', ylabel=r'$\Theta$', xlim=(-1, 1), ylim=(-3, 3))
@@ -48,7 +52,7 @@ plt.rcParams["font.size"] = square_num * 10
 #S_number_arrayの数だけsubplotを作成、長方形または正方形になるように調整
 axes = []
 for count_i in range(len(S_number_array)):
-    ax = fig.add_subplot(square_num, square_num, count_i + 1, xlabel=r'$\psi$ [$\pi$ rad]', ylabel=r'$\theta / 2 \omega_{\mathrm{t}}$', xlim=(-1, 1), ylim=(-3, 3))
+    ax = fig.add_subplot(vertical_number, horizontal_number, count_i + 1, xlabel=r'$\psi$ [$\pi$ rad]', ylabel=r'$\theta / 2 \omega_{\mathrm{t}}$', xlim=(-1, 1), ylim=(-2, 2))
     axes.append(ax)
 
 
@@ -76,22 +80,8 @@ for count_i in range(len(S_number_array)):
     ax = axes[count_i]
     ax.set_title(r'$S$ $=$ ' + f'{S_number:.2f}')
     #(a)のような図番号を付ける
-    ax.text(-0.15, 1.0, '(' + chr(97 + count_i) + ')', transform=ax.transAxes)
-
-    #各theta_0_psiを通る軌道のプロット
-    for theta_0_psi in np.linspace(theta_0_psi_min, theta_0_psi_max, theta_0_psi_num):
-        theta_plus = np.sqrt(0.5*((np.cos(wave_phase) - np.cos(theta_0_psi)) - S_number * (wave_phase - theta_0_psi)))
-        theta_minus = - np.sqrt(0.5*((np.cos(wave_phase) - np.cos(theta_0_psi)) - S_number * (wave_phase - theta_0_psi)))
-        ax.plot(wave_phase/np.pi, theta_plus, color='k', lw=1, alpha=0.3)
-        ax.plot(wave_phase/np.pi, theta_minus, color='k', lw=1, alpha=0.3)
+    ax.text(-0.20, 1.0, '(' + chr(97 + count_i) + ')', transform=ax.transAxes)
     
-    #各psi_pi_thetaを通る軌道のプロット
-    for psi_pi_theta in np.linspace(psi_pi_theta_min, psi_pi_theta_max, psi_pi_theta_num):
-        theta_plus = np.sqrt(0.5*((np.cos(wave_phase) + 1E0) + S_number * (np.pi - wave_phase)) + psi_pi_theta**2E0)
-        theta_minus = - np.sqrt(0.5*((np.cos(wave_phase) + 1E0) + S_number * (np.pi - wave_phase)) + psi_pi_theta**2E0)
-        ax.plot(wave_phase/np.pi, theta_plus, color='k', lw=1, alpha=0.3)
-        ax.plot(wave_phase/np.pi, theta_minus, color='k', lw=1, alpha=0.3)
-
     #鞍点、安定点の計算
     #非鞍点(theta = 0)の点の計算
     if S_number <= 1E0:
@@ -101,7 +91,6 @@ for count_i in range(len(S_number_array)):
         print(f'saddle_point = {saddle_point/np.pi:.2f} pi')
         non_saddle_point = non_saddle_point_iteration(S_number)
         print(f'non_saddle_point = {non_saddle_point/np.pi:.2f} pi')
-    
 
     #鞍点を通過する軌道のプロット
     if S_number <= 1E0:
@@ -119,6 +108,34 @@ for count_i in range(len(S_number_array)):
         ax.scatter(saddle_point/np.pi, 0, marker='o', color='b', s=33*square_num, zorder=100)
         ax.scatter(non_saddle_point/np.pi, 0, marker='o', color='g', s=33*square_num, zorder=100)
         ax.scatter(stable_point/np.pi, 0, marker='o', color='r', s=33*square_num, zorder=100)
+    else:
+        saddle_point = np.nan
+        non_saddle_point = np.nan
+        stable_point = np.nan
+
+    #各theta_0_psiを通る軌道のプロット
+    for theta_0_psi in np.linspace(theta_0_psi_min, theta_0_psi_max, theta_0_psi_num):
+        if S_number <= 1E0:
+            if theta_0_psi > saddle_point and theta_0_psi < stable_point:
+                continue
+            else:
+                pass
+        else:
+            pass
+        #ax.scatter(theta_0_psi/np.pi, 0, marker='o', color='purple', s=1*square_num, zorder=100)
+        theta_plus = np.sqrt(0.5*((np.cos(wave_phase) - np.cos(theta_0_psi)) - S_number * (wave_phase - theta_0_psi)))
+        theta_minus = - np.sqrt(0.5*((np.cos(wave_phase) - np.cos(theta_0_psi)) - S_number * (wave_phase - theta_0_psi)))
+        ax.plot(wave_phase/np.pi, theta_plus, color='k', lw=1, alpha=0.3, zorder=1)
+        ax.plot(wave_phase/np.pi, theta_minus, color='k', lw=1, alpha=0.3, zorder=1)
+    
+    #各psi_pi_thetaを通る軌道のプロット
+    for psi_pi_theta in np.linspace(psi_pi_theta_min, psi_pi_theta_max, psi_pi_theta_num):
+        theta_plus = np.sqrt(0.5*((np.cos(wave_phase) + 1E0) + S_number * (np.pi - wave_phase)) + psi_pi_theta**2E0)
+        theta_minus = - np.sqrt(0.5*((np.cos(wave_phase) + 1E0) + S_number * (np.pi - wave_phase)) + psi_pi_theta**2E0)
+        ax.plot(wave_phase/np.pi, theta_plus, color='k', lw=1, alpha=0.3, zorder=1)
+        ax.plot(wave_phase/np.pi, theta_minus, color='k', lw=1, alpha=0.3, zorder=1)
+
+    ax.plot([-1, 1], [0, 0], color='purple', lw=1, alpha=0.6)
     
     #psi = piを通過する軌道のプロット
     #if S_number > 0E0:
@@ -136,5 +153,6 @@ for count_i in range(len(S_number_array)):
 fig.tight_layout()
 dir_name = f'/mnt/j/KAW_simulation_data/single_test_particle/keisan/psi_theta_trajectory'
 os.makedirs(dir_name, exist_ok=True)
-fig.savefig(f'{dir_name}/psi_theta_trajectory_except_nonresonant.png')
-fig.savefig(f'{dir_name}/psi_theta_trajectory_except_nonresonant.pdf')
+fig_path = f'{dir_name}/psi_theta_trajectory_except_nonresonant_{len(S_number_array)}.png'
+fig.savefig(fig_path)
+fig.savefig(fig_path.replace('.png', '.pdf'))
